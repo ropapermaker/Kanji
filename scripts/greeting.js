5e5f09ac1105465ca8ce4395309f67f8b2f919d9
alededorigo <alededorigo@gmail.com>
2021-01-24 15:15:55 +0100

    Fixed "Good night" greeting

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -5,9 +5,9 @@ let hour = today.getHours();
 var g1 = 'おやすみなさい ';
 var g2 = 'おはようございます ';
 var g3 = 'こんにちは ';
-var g4 = 'こんばんは ';
-var g5 = 'こんばんは ';
-var g6 = 'こんばんは ';
+var g4 = 'おやすみなさい ';
+var g5 = 'おやすみなさい ';
+var g6 = 'おやすみなさい ';
 
 if (hour >= 23 && hour < 5) {
     document.getElementById('greetings').innerText = g1;
