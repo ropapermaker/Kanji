717fb88429f258eb54549bf0f6840199866f8159
Alededorigo <alededorigo@protonmail.com>
2021-03-09 15:44:02 +0100

    Created config.js, deleted config.json, improved comments, etc.

--- scripts/config.js
+++ scripts/config.js
@@ -1,43 +1,63 @@
-// Set font size
+// Main ----------------------------------------
+let image = document.getElementById('cover');
 
-// Read config.json ----------------------------
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
+image.style.borderRadius = data.image_radius;
+document.querySelector('.left').style.border = data.image_border;
+document.querySelector('.left').style.borderColor = data.image_bordercolor;
+document.querySelector('.left').style.borderRadius = data.image_radius;
+image.style.filter = data.image_filter;
+image.style.width = data.image_width;
+image.style.height = data.image_width;
+document.querySelector('.left').style.width = data.image_width;
+document.querySelector('.left').style.height = data.image_width;
+
+let icons = document.querySelectorAll('a img');
+
+if (data.enable_icons == false) {
+    for (var i = 0, max = icons.length; i < max; i++) {
+        icons[i].remove();
     }
-    rawFile.send(null);
 }
 
-// Main ----------------------------------------
-readTextFile("/config.json", function (text) {
-    var data = JSON.parse(text);
+// Greetings
+let today = new Date();
+let hour = today.getHours();
 
-    let greetings = document.getElementById('greetings');
+var g1 = data.g1;
+var g2 = data.g2;
+var g3 = data.g3;
+var g4 = data.g4;
 
-    greetings.style.fontSize = data.greeting_fontsize;
-    greetings.style.color = data.greeting_fgcolor;
+var greetings = document.getElementById('greetings');
 
-    let image = document.getElementById('cover');
+if (hour >= 23 && hour < 5) {
+    greetings.innerText = g1;
+} else if (hour >= 6 && hour < 12) {
+    greetings.innerText = g2;
+} else if (hour >= 12 && hour < 17) {
+    greetings.innerText = g3;
+} else {
+    greetings.innerText = g4;
+}
 
-    image.style.borderRadius = data.image_radius;
-    document.querySelector('.left').style.border = data.image_border;
-    document.querySelector('.left').style.borderColor = data.image_bordercolor;
-    image.style.filter = data.image_filter;
-    image.style.width = data.image_width;
-    image.style.height = data.image_width;
-    document.querySelector('.left').style.width = data.image_width;
-    document.querySelector('.left').style.height = data.image_width;
+greetings.style.fontSize = data.greeting_fontsize;
+greetings.style.color = data.greeting_fgcolor;
 
-    let icons = document.querySelectorAll('a img');
+// Clock
+var date = new Date();
 
-    if (data.enable_icons == false) {
-        for (var i = 0, max = icons.length; i < max; i++) {
-            icons[i].remove();
-        }
-    }
-});
+const label = document.getElementById('date');
+label.style.color = data.clock_fgcolor;
+label.style.fontSize = data.clock_fontsize;
+const clock = () => {
+    label.innerHTML = '<span>' + date.getHours() + ':' + date.getMinutes() + '</span>';
+    setTimeout(clock, 1000);
+};
+
+if (data.clock == true) {
+    window.onload = clock();
+} else {
+    console.log('clock is disabled');
+}
+
+document.title = data.title;
